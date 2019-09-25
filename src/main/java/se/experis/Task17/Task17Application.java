a1a0af9a3c61d7663487993792c3df99b197a24f
Zacky <zacky-kh@live.com>
2019-09-25 11:03:14 +0200

    initial commit

---
+++ src/main/java/se/experis/Task17/Task17Application.java
@@ -0,0 +1,13 @@
+package se.experis.Task17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task17Application.class, args);
+	}
+
+}
