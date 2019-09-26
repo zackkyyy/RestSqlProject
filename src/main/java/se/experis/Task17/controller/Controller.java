91633d2d4de2c54c8a70b92dc88b0ad5a23fa736
Alexandra <Bjremo>
2019-09-26 11:55:04 +0200

    new updatePerson.html file updated Controller file to match the new html file

--- src/main/java/se/experis/Task17/controller/Controller.java
+++ src/main/java/se/experis/Task17/controller/Controller.java
@@ -1,6 +1,13 @@
 package se.experis.Task17.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import se.experis.Task17.Task17Application;
+import se.experis.Task17.model.Address;
+import se.experis.Task17.model.Email;
+import se.experis.Task17.model.Person;
+import se.experis.Task17.model.PhoneNumber;
 
 /**
  * Author : Zacky Kharboutli
@@ -14,4 +21,33 @@ public class Controller {
     public String createPerson(){
         return "addPerson";
     }
+
+    @GetMapping("/update/{ID}")
+    public String updatePerson(@PathVariable int ID, Model model){
+        Person returnPerson = null;
+        Email returnEmail = null;
+        PhoneNumber returnPhoneNumber = null;
+        Address returnAddress = null;
+        for (Person person: Task17Application.people) {
+            if(person.getPersonID() == ID){
+                returnPerson = person;
+                DbHandler db = new DbHandler();
+                for (Email email:db.getALLEmails()) {
+                    if(email.getPersonID() == ID)
+                        returnEmail = email;
+                }
+                for (PhoneNumber ph:Task17Application.phoneNumbers) {
+                    if(ph.getPersonID() == ID)
+                        returnPhoneNumber = ph;
+                }
+                    returnAddress = returnPerson.getAddress();
+            }
+        }
+        model.addAttribute("person", returnPerson);
+        model.addAttribute("email", returnEmail);
+        model.addAttribute("phoneNumber", returnPhoneNumber);
+        model.addAttribute("address", returnAddress);
+        return "updatePerson";
+    }
 }
+
