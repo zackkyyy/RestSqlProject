46ef6f813ea2b09b06bf96b077c659b2f8496151
Zacky <zacky-kh@live.com>
2019-09-27 11:52:01 +0200

    Api Controller added

---
+++ src/main/java/se/experis/Task17/controller/ApiController.java
@@ -0,0 +1,48 @@
+package se.experis.Task17.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import se.experis.Task17.model.*;
+
+import java.util.ArrayList;
+
+@RestController
+public class ApiController {
+    private personController controller = new personController();
+    private DbHandler dbHandler = new DbHandler();
+
+    @GetMapping("/api/relations")
+    public ArrayList<Relationship> getAllRelations(){
+        return dbHandler.getAllRelations();
+    }
+    @GetMapping("/api/phones")
+    public ArrayList<PhoneNumber> getAllPhoneNumbers(){
+        return dbHandler.getAllPhoneNumbers();
+    }
+    @GetMapping("/api/people")
+    public ArrayList<Person> getAllPeople(){
+        return dbHandler.getAllPersons();
+    }
+    @GetMapping("/api/emails")
+    public ArrayList<Email> getAllEmails(){
+        return dbHandler.getALLEmails();
+    }
+    @GetMapping("/api/addresses")
+    public ArrayList<Address> getAllAddresses(){
+        return dbHandler.getALLAddress();
+    }
+    @GetMapping("/api/person/id/{ID}")
+    public Person personGet(@PathVariable int ID) {
+        return controller.personGet(ID);
+    }
+    @GetMapping("/api/person/name/{name}")
+    public Person customerGet(@PathVariable String name) {
+        return controller.customerGet(name);
+    }
+    @GetMapping("/api/person/number/{phoneNumber}")
+    public Person personGet(@PathVariable String phoneNumber){
+        return controller.personGet(phoneNumber);
+    }
+}
